bec9e3b9787336f851102a32779c654c21e92c9e
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-01 23:17:23 +0530

    project setup

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/', (req, res)=>{
 app.get('/about',(req,res)=>{
     res.send('Welcome to about page...')
 })
+app.get('/login', (req,res)=>{
+    res.send('<h1> Please Login Asap<h1>')
+})
+
+app.get('/contact', (req,res)=>{
+    res.send('<h2> welcome to contact page<h2>')
+})
 app.listen(port,()=>{
     console.log(`App is Listening on ${port}`);
     
