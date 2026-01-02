25791783ec97fca313c2e2318f90b3c7e39a849e
bappaditya92 <bappadityasinha.92@gmail.com>
2026-01-02 22:51:11 +0530

    commentout old code of index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const app = express();
-require('dotenv').config()
-const PORT = process.env.PORT || 5050;
+// const express = require('express');
+// const app = express();
+// require('dotenv').config()
+// const PORT = process.env.PORT || 5050;
 
-app.get('/', (req, res)=>{
-    res.send('Welcome to landing page...')
-})
+// app.get('/', (req, res)=>{
+//     res.send('Welcome to landing page...')
+// })
 
-app.get('/about',(req,res)=>{
-    res.send('Welcome to about page...')
-})
-app.get('/login', (req,res)=>{
-    res.send('<h1> Please Login Asap<h1>')
-})
+// app.get('/about',(req,res)=>{
+//     res.send('Welcome to about page...')
+// })
+// app.get('/login', (req,res)=>{
+//     res.send('<h1> Please Login Asap<h1>')
+// })
 
-app.get('/contact', (req,res)=>{
-    res.send('<h2> welcome to contact page<h2>')
-})
-app.listen(PORT,()=>{
-    console.log(`App is Listening on ${PORT}`);
+// app.get('/contact', (req,res)=>{
+//     res.send('<h2> welcome to contact page<h2>')
+// })
+// app.listen(PORT,()=>{
+//     console.log(`App is Listening on ${PORT}`);
     
-})
+// })
